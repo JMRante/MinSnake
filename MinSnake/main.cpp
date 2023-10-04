b2eb9a6f8fba20675786ae17aed64c76d9d78059
JMStark <joshuamstark@gmail.com>
2023-10-03 21:49:19 -0700

    Setup render clear with color and basic loop.

--- MinSnake/main.cpp
+++ MinSnake/main.cpp
@@ -9,11 +9,35 @@ int main(int argc, char** args) {
 		return 1;
 	}
 
-	SDL_Window* window = SDL_CreateWindow("MinSnake", 100, 100, 1920, 1080, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("MinSnake", 100, 100, 1000, 1000, SDL_WINDOW_SHOWN);
 
 	if (!window) {
 		cout << "Failed to create window: " << SDL_GetError() << "\n";
 		return 1;
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
+
+	if (!renderer) {
+		cout << "Failed to create renderer: " << SDL_GetError() << "\n";
+		return 1;
+	}
+
+	bool is_game_running = true;
+
+	while (is_game_running) {
+		Uint32 ticks = SDL_GetTicks();
+
+		if (ticks % 100 == 0) {
+			SDL_SetRenderDrawColor(renderer, 0, 0, 153, 255);
+			SDL_RenderClear(renderer);
+
+			SDL_RenderPresent(renderer);
+		}
+
+		if (ticks == 5000) {
+			is_game_running = false;
+		}
 	}
 
 	SDL_DestroyWindow(window);
