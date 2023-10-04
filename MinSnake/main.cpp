50d2446a4ab5a1253cd4890d4c783b7f08a32a9c
JMStark <joshuamstark@gmail.com>
2023-10-03 21:25:18 -0700

    Create and Delete SDL Window

--- MinSnake/main.cpp
+++ MinSnake/main.cpp
@@ -1,3 +1,24 @@
-int main() {
+#include <iostream>
+#include <SDL.h>
 
+using namespace std;
+
+int main(int argc, char** args) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		cout << "Failed to start SDL: " << SDL_GetError() << "\n";
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("MinSnake", 100, 100, 1920, 1080, SDL_WINDOW_SHOWN);
+
+	if (!window) {
+		cout << "Failed to create window: " << SDL_GetError() << "\n";
+		return 1;
+	}
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+	
+	return 0;
 }
